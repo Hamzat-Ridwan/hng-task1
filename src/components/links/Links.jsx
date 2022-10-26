1fa9a9d2fab7cd0b3e2e1a1623f3bc0f8ef8350e
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-10-26 23:17:24 +0100

    Added and styled links

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Links.css'
+
+const links = [
+    {title: 'Twitter', url: 'https://www.twitter.com', id: ''},
+    {title: 'Zuri Team', url: 'https://training.zuri.team/'},
+    {title: 'Zuri Books', url: 'http://books.zuri.team'},
+    {title: 'Python Books', url: 'https://books.zuri.team/python-for-beginners?ref_id=Ridwan Hamzat'},
+    {title: 'Background Check For Coders', url: 'https://background.zuri.team'},
+    {title: 'Design Books', url: 'https://books.zuri.team/design-rules'},
+]
 
 const Links = () => {
   return (
-    <div>Links</div>
+    <div className='links'>
+        {
+            links.map((link, index)=>(
+                <a href={link.url} key={index} >
+                    <div className='link-container'>
+                        {link.title}
+                    </div>
+                </a>
+            ))
+        }
+    </div>
   )
 }
 
