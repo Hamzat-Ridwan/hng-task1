d6c18e94d58bd22871e6cd4193c745fb4d89a5a9
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-10-27 22:43:34 +0100

    Added footer and images needed in assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Profile from './components/profile/Profile'
 import Links from './components/links/Links'
 import Socials from './components/socials/Socials'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Profile />
       <Links />
       <Socials />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ZuriLogo from '../../assets/zuri.svg'
+import IngressiveLogo from '../../assets/ingressive.svg'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div>
+            <img src={ZuriLogo} alt='zuri' />
+        </div>
+        <p>HNG Internship 9 Frontend Task</p>
+        <div>
+            <img src={IngressiveLogo} alt='ingreesive' />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
