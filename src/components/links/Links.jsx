64b57c337d3ab14ff5705c47c927e8badef9c249
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-10-29 17:12:32 +0100

    Add and hide subtexts in links

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -5,9 +5,9 @@ const links = [
     {title: 'Twitter Link', url: 'https://www.twitter.com/Hamza_rid1', id: 'twitter-link'},
     {title: 'Zuri Team', url: 'https://training.zuri.team/', id: 'btn__zuri'},
     {title: 'Zuri Books', subtext: '(This is where you will find books about design and coding)', url: 'http://books.zuri.team', id: 'books'},
-    {title: 'Python Books', url: 'https://books.zuri.team/python-for-beginners?ref_id=Ridwan Hamzat', id: 'book__python'},
-    {title: 'Background Check For Coders', url: 'https://background.zuri.team', id: 'pitch'},
-    {title: 'Design Books', url: 'https://books.zuri.team/design-rules', id: 'book__design'},
+    {title: 'Python Books', subtext: '(This is where you will get the best python books)', url: 'https://books.zuri.team/python-for-beginners?ref_id=RidwanHamzat', id: 'book__python'},
+    {title: 'Background Check For Coders', subtext: '(Your best place for doing background checks on coders)', url: 'https://background.zuri.team', id: 'pitch'},
+    {title: 'Design Books', subtext: '(Get this free design book by Zuri)', url: 'https://books.zuri.team/design-rules', id: 'book__design'},
 ]
 
 const Links = () => {
@@ -15,7 +15,7 @@ const Links = () => {
     <div className='links'>
         {
             links.map((link, index)=>(
-                <a href={link.url} key={index} id={link.id}>
+                <a href={link.url} key={index} id={link.id} title>
                     <div className='link-container'>
                         {link.title}
                         {link.subtext? 
