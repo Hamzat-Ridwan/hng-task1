7039d78397689ea3baed70165a26cf04c1d0bcf4
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-10-29 14:38:12 +0100

    Add subtext to zuri books

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -4,7 +4,7 @@ import './Links.css'
 const links = [
     {title: 'Twitter Link', url: 'https://www.twitter.com/Hamza_rid1', id: 'twitter-link'},
     {title: 'Zuri Team', url: 'https://training.zuri.team/', id: 'btn__zuri'},
-    {title: 'Zuri Books', url: 'http://books.zuri.team', id: 'books'},
+    {title: 'Zuri Books', subtext: '(This is where you will find books about design and coding)', url: 'http://books.zuri.team', id: 'books'},
     {title: 'Python Books', url: 'https://books.zuri.team/python-for-beginners?ref_id=Ridwan Hamzat', id: 'book__python'},
     {title: 'Background Check For Coders', url: 'https://background.zuri.team', id: 'pitch'},
     {title: 'Design Books', url: 'https://books.zuri.team/design-rules', id: 'book__design'},
